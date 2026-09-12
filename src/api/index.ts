2fd62aeaaca8e5ee0bd8a8a0c0cb9fe8127a04f7
maurolnl <mauroleoquiroga@gmail.com>
2026-09-12 14:13:14 -0300

    refactor(api): use REST employee repository

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,26 +1,6 @@
 import { EmployeeRepository } from "@/features/employees/repo/employee-repository";
-import { userRepositorySupabase } from "@/features/employees/repo/employee-repository.supabase";
 import { employeeRepositoryRest } from "@/features/employees/repo/rest/employee-repository.rest";
 
-enum ApiProvider {
-  subapase = "supabase",
-  dedicated = "dedicated",
-}
-
-const serviceProvider = (import.meta.env.VITE_API_PROVIDER ||
-  "supabase") as ApiProvider;
-
-const getProvider = (provider: ApiProvider) => {
-  switch (provider) {
-    case ApiProvider.subapase:
-      return userRepositorySupabase;
-    case ApiProvider.dedicated:
-      return employeeRepositoryRest;
-    default:
-      return employeeRepositoryRest;
-  }
-};
-
-const employeeRepository: EmployeeRepository = getProvider(serviceProvider);
+const employeeRepository: EmployeeRepository = employeeRepositoryRest;
 
 export { employeeRepository };
