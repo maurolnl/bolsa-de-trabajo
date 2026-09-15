15c04585060f306b3dc86a1ee5ecbb746c9fca07
maurolnl <mauroleoquiroga@gmail.com>
2026-09-15 16:05:34 -0300

    feat(employer): implement profile REST repository

--- src/features/employers/repo/rest/employer-repository.rest.ts
+++ src/features/employers/repo/rest/employer-repository.rest.ts
@@ -1,10 +1,17 @@
 import { httpClient } from "@/core/services/httpClient";
 
 import { EmployerRepository } from "../employer-repository";
+import { mapCreateEmployer, mapEmployerResponse } from "./helpers";
+import { EmployerResponse } from "./types";
 
 export const employerRepositoryRest: EmployerRepository = {
   getByUserId: async (userId) => {
-    const { data } = await httpClient.get(`users/${userId}/employer`);
-    return data;
+    const { data } = await httpClient.get<EmployerResponse>(
+      `users/${userId}/employer`,
+    );
+    return mapEmployerResponse(data);
+  },
+  createEmployer: async (employer) => {
+    await httpClient.post("employers", mapCreateEmployer(employer));
   },
 };
